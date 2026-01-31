34a0abe9acedd9500d38016fd7cf0f0693ed1a05
Bruno Moreira GAB <brunohenriquetorres99@gmail.com>
2026-01-31 19:32:55 -0300

    INCLUIR VALIDAÇÃO WHILE

--- js/app.js
+++ js/app.js
@@ -4,12 +4,34 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if(imagem.classList.contains('dashboard__item__img--rented')) {
+        let confirmacao ='';
+
+        do{
+
+            confirmacao = prompt('Deseja devolver o jogo? Aperta 1 para SIM e 2 para Não.') 
+
+        } while(confirmacao < 1 || confirmacao > 2 || isNaN(confirmacao));
+
+        if(confirmacao === 1){
+
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+
+        }else{
+
+            alert('Aproveite o jogo.');
+
+        };
+        
+
     }else{
+       
+        
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-    }
-}
+
+        
+    };
+};
